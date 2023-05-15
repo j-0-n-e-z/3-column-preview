f588e7eb9c1bf70f81f5c8c6793b35dccf5edbe7
j-0-n-e-z <j000neeez@gmail.com>
2023-05-15 14:39:37 +0500

    refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 module.exports = {
 	content: ['./dist/**/*.{html,js}'],
 	theme: {
@@ -9,6 +11,13 @@ module.exports = {
 				veryDarkCyan: 'hsl(179, 100%, 13%)',
 				transparentWhite: 'hsla(0, 0%, 100%, 0.75)',
 				veryLightGray: 'hsl(0, 0%, 95%)'
+			},
+			fontFamily: {
+				LexendDeca: ['Lexend Deca', ...defaultTheme.fontFamily.sans],
+				BigShouldersDisplay: [
+					'Big Shoulders Display',
+					...defaultTheme.fontFamily.sans
+				]
 			}
 		}
 	},
